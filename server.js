aeb10b6073e58fe9a20b2e68bc9b47d23009752b
DavidMax <davidjamesmax@gmail.com>
2016-01-10 19:54:13 -0800

    pass mongoose path of local mongo db

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-mongoose.connect('');
+mongoose.connect('mongodb://localhost:27017/mean-social');
 
 app.get('/', function(req, res){
+    // sendfile() is deprecated in favor of sendFile() in Express 4
     res.sendfile('index.html');
 });
 
 app.listen('3000', function(){
-   console.log('Listening for Local Host 3000');
+   console.log('Listening on port 3000');
 });
