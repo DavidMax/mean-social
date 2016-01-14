74263dc8b8463e66d826ecbf6b1434891182e031
DavidMax <davidjamesmax@gmail.com>
2016-01-13 21:58:14 -0800

    Stub out Auth API; add express.static middleware

--- server.js
+++ server.js
@@ -3,13 +3,22 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 var app = express();
+var authenticationController = require('./server/controllers/authentication/authentication-controller');
 
 mongoose.connect('mongodb://localhost:27017/mean-social');
+
+app.use(bodyParser.json());
+
+app.use('/app', express.static(__dirname + '/app'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 app.get('/', function(req, res){
     // sendfile() is deprecated in favor of sendFile() in Express 4
     res.sendfile('index.html');
 });
+
+// Authentication
+app.post('/api/user/signup', authenticationController.signup);
 
 app.listen('3000', function(){
    console.log('Listening on port 3000');
